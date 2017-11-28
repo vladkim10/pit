a84b420049c4cb5db2aab890c466ecd3777192cf
admin <vlad.kim.10@gmail.com>
2017-11-28 10:26:32 +0600

    added age field

--- pit/models.py
+++ pit/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Dog(models.Model):
   name = models.CharField(max_length=1000,default="Sharik")
   breed = models.CharField(max_length=1000,default="Dvor")
+  age = models.IntegerField(default=1)
 
   def __str__(self):
     return self.name
