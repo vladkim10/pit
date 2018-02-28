ad1e14b2fcfb3e187888afbe88f8495c194b71a3
student <vlad.kim.10@gmail.com>
2018-02-28 15:19:19 +0600

    donate

--- pit/urls.py
+++ pit/urls.py
@@ -5,6 +5,7 @@ from . import views_kkb
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^transactions$', views.kzt_list, name='kzt_list'),
+    url(r'^success$', views.success, name='success'),
     url(r'^transactions/new/$', views.transaction_new, name='transaction_new'),
     url(r'^dog/new/$', views.dog_new, name='dog_new'),
     url(r'^donate$', views_kkb.kkb_init, name='kkb_init'),
